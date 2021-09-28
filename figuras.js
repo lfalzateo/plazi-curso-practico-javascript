91769006ecae2a20586aa130c6f4031ebacd4abe
Luis Felipe Alzate Osorio <pipealzate546@hotmail.com>
2021-09-28 10:16:47 -0500

    Primera prueba del commit

---
+++ figuras.js
@@ -0,0 +1,20 @@
+//Código del cuadrado
+console.group('Cuadrados');
+const lado_cuadrado=5;
+console.log('Los lados del cuadrado miden '+lado_cuadrado);
+
+const perimetro_cuadrado=lado_cuadrado*4;
+console.log("El perímetro del cuadrado es "+perimetro_cuadrado);
+
+const area_cuadrado=lado_cuadrado**2;
+console.log('El area del cuadrado es perimetro_cuadrado '+ area_cuadrado);
+
+console.groupEnd();
+
+//código del triángulo
+console.group('Triangulos');
+const lado1_triangulo=6;
+const lado2_triangulo=10;
+const base_triangulo=15;
+console.log('Los lados del triangulo miden '+lado1_triangulo+ '-'+lado2_triangulo+'-'+base_triangulo);
+console.groupEnd();
